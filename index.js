c209b5063aad7db3b7079fa8cc3e41c8c1e151e1
Jesse Baker <jcb.121@hotmail.co.uk>
2017-01-11 21:22:49 +0100

    added react

--- index.js
+++ index.js
@@ -2,19 +2,15 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/build'));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send('index');
 });
 
 app.post('/', function(request, response){
-  response.render('pages/index');
+  response.send('index');
 })
 
 app.listen(app.get('port'), function() {
