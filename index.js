5d72c0c90ce449fbfd962e4b5038c79ae18d2173
Jesse Baker <jcb.121@hotmail.co.uk>
2017-01-11 20:45:34 +0100

    post method for fb

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/', function(request, response){
+  response.render('pages/index');
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
